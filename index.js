1f233ec171052857f2d3ec0b210323b5af3ba054
Aryan Godara <aryangodara03@gmail.com>
2023-12-10 00:56:35 +0530

    add multer file upload middleware

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-// index.js
 const express = require('express');
 const multer = require('multer');
 const app = express();
-const port = 3000; // You can use any available port
+const port = 3500; // You can use any available port
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+// Multer Configuration
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+// File Upload Endpoint
+app.post('/upload', upload.single('file'), (req, res) => {
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded' });
+    }
+    res.json({ message: 'File uploaded successfully', filename: req.file.filename });
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
