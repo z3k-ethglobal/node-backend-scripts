89fe083ba4c95d47fb866d16ba0069c92ee5eba6
Aryan Godara <aryangodara03@gmail.com>
2023-12-10 01:03:37 +0530

    complete post API for file upload

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const app = express();
-const port = 3500; // You can use any available port
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+const port = 8080; // You can use any available port
 
 // Multer Configuration
 const storage = multer.diskStorage({
@@ -19,18 +15,14 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
 // File Upload Endpoint
 app.post('/upload', upload.single('file'), (req, res) => {
-    if (!req.file) {
-      return res.status(400).json({ error: 'No file uploaded' });
-    }
-    res.json({ message: 'File uploaded successfully', filename: req.file.filename });
-  });
-  
-  app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-  });
+if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+}
+res.json({ message: 'File uploaded successfully', filename: req.file.filename });
+});
+
+app.listen(port, () => {
+console.log(`Server is running on http://localhost:${port}`);
+});
